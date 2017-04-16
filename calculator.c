7378ff4a1954a09c3b998c158ff4060daa05a729
Shubham Jain <jain2304shubham@gmail.com>
2017-04-16 21:07:46 +0530

    function in c language

---
+++ calculator.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+sum(int x ,int y)
+{
+	int z;
+	no();
+	z=x+y;
+	return z;
+}
+sub(int x ,int y)
+{
+	int p;
+	no();
+	p=x-y;
+	return p;
+}
+mul(int x ,int y)
+{
+	int q;
+	no();
+	q=x*y;
+	return q;
+}
+div(float x ,float y)
+{
+	int r;
+	no();
+	r=x/y;
+	return r;
+}
+no(void)
+{
+	int a,b;
+	printf(" enter the value ");
+	scanf("%d%d",&a,&b);
+}
+int main()
+{
+	int a,b,c,result;
+	printf(" enter the operator which u want to perform +, -,*,/");
+	scanf("%c", &c);
+	no();
+	switch(c)
+	{
+		case '+':
+			{
+				int result;
+				result=sum(int a,int b);
+				printf("result=%d",result);
+				break;
+			}
+		case '-':
+			{
+				result=sub(int a,int b);
+				printf("result=%d",result);
+				break;
+			}
+		case '*':
+			{
+				result=mul(int a,int b);
+				printf("result=%d",result);
+				break;
+			}
+		case '/':
+			{
+				result=div(float a,float b);
+				printf("result=%d",result);
+				break;
+			}
+	}
+	default:
+		{
+			printf("error");
+		}
+	return 0;
+}
